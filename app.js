ec7a83453e522e9a0de495f243916451f9df9e5a
Sagar <sagarmaharjan31@gmail.com>
2018-12-25 15:47:41 +0545

    Forms & validation errors

--- app.js
+++ app.js
@@ -4,10 +4,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 
-// /* Static user info
+/* Static user info */
 var users = [
     {
         first_name: 'Sagar',
@@ -42,7 +43,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 /* Middleware for public/static folder */
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Using app.get because we wanna handle the get request
+/* Global vars */
+app.use(function (req, res, next) {
+    res.locals.errors = null;
+    next();
+});
+
+/* Middleware for express-validator */
+app.use(expressValidator());
+
+/* Using app.get because we wanna handle the get request */
 app.get('/', function (req, res) {
     // res.send('Hello World');
     // res.json(users);
@@ -52,7 +62,35 @@ app.get('/', function (req, res) {
     });
 });
 
-// To run our application we need to listen to a port
+/* Using app.post because we wanna handle the post request */
+app.post('/users/add', function (req, res) {
+    // console.log(req.body.first_name);
+    // Setting rules for a field
+    req.checkBody('first_name', 'First name is required').notEmpty();
+    req.checkBody('last_name', 'Last name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+
+    // Validation Errors
+    var errors = req.validationErrors();
+
+    if (errors) {
+        console.log('ERRORS!');
+        res.render('index', {
+            title: 'Employees',
+            users: users,
+            errors: errors
+        });
+    } else {
+        var newUser = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email
+        }
+        console.log(newUser);
+    }
+});
+
+/* To run our application we need to listen to a port */
 app.listen(5000, function () {
     console.log('Server started on 5000..');
 });
